cce4c49ad3820f27adc4a0ab6650ec221c5280c5
I <info@remcotolsma.nl>
2016-09-21 11:18:11 +0200

    Fixed tabs.

--- templates/company_sections.php
+++ templates/company_sections.php
@@ -11,8 +11,8 @@ if ( ! empty( $sections ) ) : ?>
 
 				<?php foreach ( $sections as $section ) : ?>
 
-					<li class="<?php echo esc_attr( $active ? 'active' : '' ); ?>">
-						<a href="#<?php echo esc_attr( $section['id'] ); ?>"><?php echo esc_html( $section['name'] ); ?></a>
+					<li class="nav-item">
+						<a href="#<?php echo esc_attr( $section['id'] ); ?>" class="nav-link <?php echo esc_attr( $active ? 'active' : '' ); ?>" ><?php echo esc_html( $section['name'] ); ?></a>
 					</li>
 
 					<?php $active = false; ?>
