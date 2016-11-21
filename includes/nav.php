87ab6a6d3cbb9b13005d1091dc8d566accfa2b37
Remco Tolsma <info@remcotolsma.nl>
2016-11-21 11:50:57 +0100

    Removed Robbery nav menu CS class for custom menu items.

--- includes/nav.php
+++ includes/nav.php
@@ -22,21 +22,3 @@ function orbis_nav_menu_icons( $classes, $item, $args ) {
 }
 
 add_filter( 'nav_menu_css_class', 'orbis_nav_menu_icons', 10, 3 );
-
-/**
- * Active archive links
- */
-function robbery_nav_menu_css_class( $classes, $item ) {
-	if ( 'custom' === $item->type ) {
-		$is_ancestor = strncmp( get_permalink(), $item->url, strlen( $item->url ) ) === 0;
-		$is_home = untrailingslashit( $item->url ) === home_url();
-
-		if ( $is_ancestor && ! $is_home ) {
-			$classes[] = 'current-url-ancestor';
-		}
-	}
-
-	return $classes;
-}
-
-add_filter( 'nav_menu_css_class', 'robbery_nav_menu_css_class', 10, 2 );
