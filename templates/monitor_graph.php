f3466aea820dc27cf26e44e7d2cb867e37f1e3ea
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-16 09:04:17 +0200

    Made monitor response time graph more efficient.

--- templates/monitor_graph.php
+++ templates/monitor_graph.php
@@ -6,36 +6,32 @@ global $wpdb;
 
 $response_times = $wpdb->get_results( $wpdb->prepare( "
 	SELECT
-		duration,
+		AVG( duration ) AS avg_duration,
 		monitored_date
 	FROM
 		$wpdb->orbis_monitor_responses
 	WHERE
 		post_id = %d
+	GROUP BY 
+		WEEKOFYEAR( monitored_date )
 	ORDER BY
 		monitored_date ASC
-", $post->ID ) );
+",
+	$post->ID
+) );
 
 foreach ( $response_times as $response ) {
-	$date   = new DateTime( $response->monitored_date );
-	$period = $date->format( "W" );
-
-	$all_period_durations[ $period ][] = (float) $response->duration;
+	$date = strtotime( $response->monitored_date ) * 1000; // WPCS: precision alignment ok.
+	$average_period_durations[] = [ $date, $response->avg_duration ];
 }
 
-foreach ( $all_period_durations as $period => $period_durations ) {
-	$total = array_sum( $period_durations );
-	$average_duration = $total / count( $period_durations );
-
-	$average_period_durations[] = [ $period, $average_duration ];
-}
-
-$response_times_json = wp_json_encode( $average_period_durations );
+$response_times_json = wp_json_encode( $average_period_durations, JSON_NUMERIC_CHECK );
 ?>
 
 <div class="card mb-3">
-	<div class="card-header"><?php esc_html_e( 'Monitor Graph - Average Response Time Per Week', 'orbis' ); ?></div>
+	<div class="card-header"><?php esc_html__( 'Monitor Graph - Average Response Time Per Time Period', 'orbis' ); ?></div>
 	<div class="card-body">
+
 		<div id="graph" style="width:100%;height:300px"></div>
 	</div>
 
@@ -43,15 +39,15 @@ $response_times_json = wp_json_encode( $average_period_durations );
 		jQuery( document ).ready( function( $ ) {
 			var options = {
 				xaxis: {
-					minTickSize: 1,
-					tickDecimals: 0
+					mode: "time",
+					timeformat: "%d/%m"
 				},
 				series: {
 					lines: { show: true },
 					points: { show: true }
 				}
 			}
-			$.plot( "#graph", [<?php echo $response_times_json; ?>], options );
+			$.plot( "#graph", [<?php echo wp_kses_post( $response_times_json ); ?>], options );
 		} );
 	</script>
 </div>
