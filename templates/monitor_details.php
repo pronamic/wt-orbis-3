d89ad6f2bef2ff92bc3acf03a95d4d3cbca44193
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-17 11:09:44 +0200

    Added clickable link to monitor details.

--- templates/monitor_details.php
+++ templates/monitor_details.php
@@ -9,8 +9,13 @@
 					<?php
 
 					$url = get_post_meta( get_the_ID(), '_orbis_monitor_url', true );
+					$link = sprintf(
+						'<a href="%s">%s</a>',
+						$url,
+						$url
+					);
 
-					echo empty( $url ) ? '—' : esc_html( $url );
+					echo empty( $url ) ? '—' : wp_kses_post( $link );
 
 					?>
 				</dd>
