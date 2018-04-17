7c1f6edcaed20c5960293554407ac8f67f291e78
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-17 11:40:13 +0200

    Added response amount filter

--- templates/monitor_responses.php
+++ templates/monitor_responses.php
@@ -2,6 +2,8 @@
 
 global $wpdb;
 
+$amount = isset( $_GET['period'] ) ? sanitize_text_field( wp_unslash( $_GET['amount'] ) ) : '10';
+
 $responses = $wpdb->get_results( $wpdb->prepare( "
 	SELECT
 		*
@@ -12,64 +14,78 @@ $responses = $wpdb->get_results( $wpdb->prepare( "
 	ORDER BY
 		monitored_date DESC
 	LIMIT
-		0, 50
-", $post->ID ) );
+		0, %d
+",
+$post->ID,
+$amount
+) );
+
+$table_title = __( 'Last', 'orbis' ) . ' ' . $amount . ' ' . __( 'Responses', 'orbis' );
+$label = $amount . ' ' . __( 'Responses', 'orbis' );
 
 ?>
 
 <div class="card mb-3">
-	<div class="card-header"><?php esc_html_e( 'Monitor Responses', 'orbis' ); ?></div>
-
-		<div class="table-responsive">
-			<table class="table table-striped mb-0">
-				<thead>
-					<tr>
-						<th class="border-top-0" scope="col"><?php esc_html_e( 'Date', 'orbis' ); ?></th>
-						<th class="border-top-0" scope="col"><?php esc_html_e( 'Duration', 'orbis' ); ?></th>
-						<th class="border-top-0" scope="col"><?php esc_html_e( 'Code', 'orbis' ); ?></th>
-						<th class="border-top-0" scope="col"><?php esc_html_e( 'Message', 'orbis' ); ?></th>
-						<th class="border-top-0" scope="col"><?php esc_html_e( 'Content Length', 'orbis' ); ?></th>
-						<th class="border-top-0" scope="col"><?php esc_html_e( 'Content Type', 'orbis' ); ?></th>
-					</tr>
-				</thead>
-
-				<tbody>
-
-					<?php foreach ( $responses as $response ) : ?>
-
-						<tr>
-							<td>
-								<?php echo esc_html( $response->monitored_date ); ?>
-							</td>
-							<td>
-								<?php
-
-								if ( empty( $response->duration ) ) {
-									echo '—';
-								} else {
-									echo esc_html( number_format_i18n( $response->duration, 6 ) );
-								}
-
-								?>
-							</td>
-							<td>
-								<?php echo esc_html( $response->response_code ); ?>
-							</td>
-							<td>
-								<?php echo esc_html( $response->response_message ); ?>
-							</td>
-							<td>
-								<?php echo esc_html( $response->response_content_length ); ?>
-							</td>
-							<td>
-								<?php echo esc_html( $response->response_content_type ); ?>
-							</td>
-						</tr>
-
-					<?php endforeach; ?>
-
-				</tbody>
-			</table>
+	<div class="card-header">
+		<?php echo esc_html( $table_title ); ?>
+		<button class="btn btn-light dropdown-toggle m-0 p-0 float-right" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo esc_html( $label ); ?></button>
+		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			<a class="dropdown-item" href="<?php echo esc_url( add_query_arg( 'amount', '10' ) ); ?>"><?php echo esc_html( '10' ); ?></a>
+			<a class="dropdown-item" href="<?php echo esc_url( add_query_arg( 'amount', '50' ) ); ?>"><?php echo esc_html( '50' ); ?></a>
+			<a class="dropdown-item" href="<?php echo esc_url( add_query_arg( 'amount', '100' ) ); ?>"><?php echo esc_html( '100' ); ?></a>
 		</div>
+	</div>
+
+	<div class="table-responsive">
+		<table class="table table-striped mb-0">
+			<thead>
+				<tr>
+					<th class="border-top-0" scope="col"><?php esc_html_e( 'Date', 'orbis' ); ?></th>
+					<th class="border-top-0" scope="col"><?php esc_html_e( 'Duration', 'orbis' ); ?></th>
+					<th class="border-top-0" scope="col"><?php esc_html_e( 'Code', 'orbis' ); ?></th>
+					<th class="border-top-0" scope="col"><?php esc_html_e( 'Message', 'orbis' ); ?></th>
+					<th class="border-top-0" scope="col"><?php esc_html_e( 'Content Length', 'orbis' ); ?></th>
+					<th class="border-top-0" scope="col"><?php esc_html_e( 'Content Type', 'orbis' ); ?></th>
+				</tr>
+			</thead>
+
+			<tbody>
+
+				<?php foreach ( $responses as $response ) : ?>
+
+					<tr>
+						<td>
+							<?php echo esc_html( $response->monitored_date ); ?>
+						</td>
+						<td>
+							<?php
+
+							if ( empty( $response->duration ) ) {
+								echo '—';
+							} else {
+								echo esc_html( number_format_i18n( $response->duration, 6 ) );
+							}
+
+							?>
+						</td>
+						<td>
+							<?php echo esc_html( $response->response_code ); ?>
+						</td>
+						<td>
+							<?php echo esc_html( $response->response_message ); ?>
+						</td>
+						<td>
+							<?php echo esc_html( $response->response_content_length ); ?>
+						</td>
+						<td>
+							<?php echo esc_html( $response->response_content_type ); ?>
+						</td>
+					</tr>
+
+				<?php endforeach; ?>
+
+			</tbody>
+		</table>
+	</div>
 
 </div>
