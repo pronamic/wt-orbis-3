17da721d1bcee279c302e0b2db4cbedee276adfa
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-25 14:54:35 +0200

    WordPress Coding Standards.

--- archive-orbis_monitor.php
+++ archive-orbis_monitor.php
@@ -76,9 +76,9 @@ get_header();
 								<?php
 								if ( get_post_meta( $post->ID, '_orbis_monitor_response_date' ) ) {
 									$time = esc_html( get_post_meta( $post->ID, '_orbis_monitor_response_date', true ) );
-									if ( $time !== "" ) {
+									if ( '' !== $time ) {
 										echo esc_html( date( 'd-m-Y H:i', $time ) );
-									} else{
+									} else {
 										echo '-';
 									}
 								}
